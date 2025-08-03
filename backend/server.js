fbdb828b8b066695b8fa6ccfe42d0067144d743f
Polisetti Shruti <polishettyshruti@gmail.com.com>
2025-08-03 14:32:43 +0530

    Update: cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const app = express();
 
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://authentication-frontend-pcu9.onrender.com' 
+}));
 app.use(express.json());
 
 // Connect MongoDB
