1131ba80421aee9877b4d6e726febae3657d7387
Polisetti Shruti <polishettyshruti@gmail.com.com>
2025-08-03 12:24:45 +0530

    Fix: remove deprecated MongoDB options

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect MongoDB
-mongoose.connect(MONGO_URL)
+mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
